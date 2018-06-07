aa53242eae3afebe2027063660580be8a23e889a
AnaRadojevic <anaradojevic90@gmail.com>
2018-06-07 18:54:46 +0200

    drugi commit, dodata nova klasa Radio.java, kao i odgovarajuci atributi i get/set metode

---
+++ src/media/Radio.java
@@ -0,0 +1,37 @@
+
+package media;
+
+
+public class Radio {
+    
+    private double fmFrequency;
+    private int amFrequency;
+    private char band;
+    
+    public double getFmFrequency() {
+        return this.fmFrequency;
+    }
+    
+    public void setFmFrequency(double fmFrequency) {
+        this.fmFrequency = fmFrequency;
+    }
+    
+    public int getAmFrequency() {
+        return this.amFrequency;
+    }
+    
+    public void setAmFrequency(int amFrequency) {
+        this.amFrequency = amFrequency;
+    }
+    
+    public char getBand() {
+        return this.band;
+    }
+    
+    public void setBand(char band) {
+        this.band = band;
+    }
+    
+    
+    
+}
